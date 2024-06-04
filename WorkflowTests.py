2f40f799e0545353fb61d44616e1851a2e7b6367
Diana Flodin <diana.flodin@yh.nackademin.se>
2024-06-04 13:12:53 +0200

    första commit

---
+++ WorkflowTests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class WorkflowTests(unittest.TestCase):
+          
+    def test_workflow_should_always_pass(self):
+        self.assertTrue(True)
+
+if __name__ == '_main_':
+   unittest.main()
